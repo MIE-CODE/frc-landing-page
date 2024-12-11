5521e1c6fa6fc447bea58dcd97682975e9d981bc
Meyangz <Kayfolayan614@gmail.com>
2024-12-11 14:03:25 +0100

    made faq dropdowmn component

--- src/component/body/animation/dropdown.jsx
+++ src/component/body/animation/dropdown.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 export default function Dropdown({ heading, body }) {
   const [open, setOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
-    <div className="body-faq-item">
+    <div ref={dropdownRef} className="body-faq-item">
       <div onClick={() => setOpen(!open)} className="body-faq-item-md">
         {heading}
       </div>
